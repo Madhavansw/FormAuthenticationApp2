30fc38f404351780464eb281acaaff9556f4ab97
Madhavan <50607946+Madhavansw@users.noreply.github.com>
2023-05-11 19:30:35 +0530

    Add files via upload

---
+++ FormValidController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DropDownApp.Models;
+using DropDownApp.DataModels;
+
+namespace DropDownApp.Controllers
+{
+    public class FormValidController : Controller
+    {
+        // GET: FormValid
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ActionName("SiteCreateMng")]
+        public ActionResult SiteCreateMng( SiteMng mng)
+        {
+
+
+
+            return Json(new { Msg = true }, JsonRequestBehavior.AllowGet);
+
+        }
+    }
+}
